c4bfa9c40d9775ca3fe25f63eabf1fd4e1d22dc1
Chronos-Ye <47781909+Chronos-Ye@users.noreply.github.com>
2020-06-23 14:40:28 +0800

    Update Log.java

--- src/main/java/com/tencentcloudapi/common/Log.java
+++ src/main/java/com/tencentcloudapi/common/Log.java
@@ -14,13 +14,12 @@ import com.squareup.okhttp.Response;
 class Log implements Interceptor {
   private boolean debug;
   private Logger logger = null;
-  private static Log log = null;
-
-  private Log() {
+  
+  public Log() {
     this(false);
   }
 
-  private Log(boolean isDebug) {
+  public Log(boolean isDebug) {
     File file = new File("log4j.properties");
     if (!file.exists()) {
       this.debug = false;
@@ -30,28 +29,6 @@ class Log implements Interceptor {
     logger = Logger.getLogger(this.getClass().getName());
     PropertyConfigurator.configure("log4j.properties");
     logger.setLevel(Level.ALL);
-  }
-
-  public static Log getLog(boolean isDebug) {
-    if (log == null) {
-      synchronized (Log.class) {
-        if (log == null) {
-          log = new Log(isDebug);
-        }
-      }
-    }
-    return log;
-  }
-
-  public static Log getLog() {
-    if (log == null) {
-      synchronized (Log.class) {
-        if (log == null) {
-          log = new Log(false);
-        }
-      }
-    }
-    return log;
   }
 
   public void info(final String str) {
@@ -84,11 +61,11 @@ class Log implements Interceptor {
     Request request = chain.request();
     String req = ("send request, request url: " + request.urlString() + ". request headers information: " + request.headers().toString());
     req = req.replaceAll("\n", ";");
-    getLog().debug(req);
+    this.debug(req);
     Response response = chain.proceed(request);
     String resp = ("recieve response, response url: " + response.request().urlString() + ", response headers: " + response.headers().toString() + ",response body information: " + response.body().toString());
     resp = resp.replaceAll("\n", ";");
-    getLog().debug(resp);
+    this.debug(resp);
     return response;
   }
 
