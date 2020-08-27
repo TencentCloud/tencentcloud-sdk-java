b7f65819ba14c056a6bf31d16486040c23a527e7
heye <heye@tencent.com>
2020-08-27 16:20:50 +0800

    返回错误码修正

--- src/main/java/com/tencentcloudapi/common/exception/TencentCloudSDKException.java
+++ src/main/java/com/tencentcloudapi/common/exception/TencentCloudSDKException.java
@@ -53,11 +53,12 @@ public class TencentCloudSDKException extends Exception {
     return errorCode;
   }
 
+  @Override
   public String toString() {
     return "[TencentCloudSDKException]"
-        + " code:"
-        +this.getErrorCode()
-        + "message:"
+        + "code:"
+        + this.getErrorCode()
+        + " message:"
         + this.getMessage()
         + " requestId:"
         + this.getRequestId();
