f21eddcb825705075d6c8f62cadc86723fba05dd
tencentcloudapi <tencentcloudapi@tenent.com>
2021-06-11 11:48:26 +0800

    release 3.1.288

--- src/main/java/com/tencentcloudapi/common/AbstractClient.java
+++ src/main/java/com/tencentcloudapi/common/AbstractClient.java
@@ -59,7 +59,7 @@ import com.google.gson.JsonSyntaxException;
 public abstract class AbstractClient {
 
   public static final int HTTP_RSP_OK = 200;
-  public static final String SDK_VERSION = "SDK_JAVA_3.1.287";
+  public static final String SDK_VERSION = "SDK_JAVA_3.1.288";
 
   private Credential credential;
   private ClientProfile profile;
