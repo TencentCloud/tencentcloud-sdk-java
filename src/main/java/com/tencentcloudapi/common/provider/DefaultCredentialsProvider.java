77461f78752479716253316ce9023020347cd591
willhhan <sesky4@gmail.com>
2023-08-03 19:32:50 +0800

    feat: add OIDCRoleArnProvider

--- src/main/java/com/tencentcloudapi/common/provider/DefaultCredentialsProvider.java
+++ src/main/java/com/tencentcloudapi/common/provider/DefaultCredentialsProvider.java
@@ -3,6 +3,8 @@ package com.tencentcloudapi.common.provider;
 import com.tencentcloudapi.common.Credential;
 import com.tencentcloudapi.common.exception.TencentCloudSDKException;
 
+import java.io.IOException;
+
 public class DefaultCredentialsProvider implements CredentialsProvider {
     @Override
     public Credential getCredentials() throws TencentCloudSDKException {
@@ -23,6 +25,13 @@ public class DefaultCredentialsProvider implements CredentialsProvider {
         if (cred.getSecretId() != null && cred.getSecretKey() != null && cred.getToken() != null) {
             return cred;
         }
+
+        try {
+            cred = new OIDCRoleArnProvider().getCredentials();
+            return cred;
+        } catch (IOException e) {
+        }
+
         throw new TencentCloudSDKException("No valid credential");
     }
 }
