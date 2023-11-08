60d2d8796bb0ef32c23924f5dc0cb11398d34676
tencentcloudapi <tencentcloudapi@tencent.com>
2023-11-08 17:31:52 +0800

    fix: remove temp-directory

--- temp-directory/tencentcloud-sdk-java-common/src/main/java/com/tencentcloudapi/common/provider/CredentialsProvider.java
+++
@@ -1,8 +0,0 @@
-package com.tencentcloudapi.common.provider;
-
-import com.tencentcloudapi.common.Credential;
-import com.tencentcloudapi.common.exception.TencentCloudSDKException;
-
-public interface CredentialsProvider {
-    public Credential getCredentials() throws TencentCloudSDKException;
-}
