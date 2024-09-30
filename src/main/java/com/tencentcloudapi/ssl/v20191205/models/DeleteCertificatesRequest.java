ba49d0ce342270be3cf179e6cb275c93876cf7b6
tencentcloudapi <tencentcloudapi@tencent.com>
2024-10-01 04:25:35 +0800

    release 3.1.1117

--- src/main/java/com/tencentcloudapi/ssl/v20191205/models/DeleteCertificatesRequest.java
+++ src/main/java/com/tencentcloudapi/ssl/v20191205/models/DeleteCertificatesRequest.java
@@ -23,6 +23,52 @@ import java.util.HashMap;
 
 public class DeleteCertificatesRequest extends AbstractModel {
 
+    /**
+    * 要删除的证书ID。单次最多100个
+    */
+    @SerializedName("CertificateIds")
+    @Expose
+    private String [] CertificateIds;
+
+    /**
+    * 删除时是否检查证书关联了云资源。默认不检查。如需要检查关联云资源 (需授权服务角色SSL_QCSLinkedRoleInReplaceLoadCertificate)，完成授权后，删除将变成异步任务，接口会返回异步任务ID。需搭配 DescribeDeleteCertificatesTaskResult接口使用，查询删除任务是否成功。
+    */
+    @SerializedName("IsSync")
+    @Expose
+    private Boolean IsSync;
+
+    /**
+     * Get 要删除的证书ID。单次最多100个 
+     * @return CertificateIds 要删除的证书ID。单次最多100个
+     */
+    public String [] getCertificateIds() {
+        return this.CertificateIds;
+    }
+
+    /**
+     * Set 要删除的证书ID。单次最多100个
+     * @param CertificateIds 要删除的证书ID。单次最多100个
+     */
+    public void setCertificateIds(String [] CertificateIds) {
+        this.CertificateIds = CertificateIds;
+    }
+
+    /**
+     * Get 删除时是否检查证书关联了云资源。默认不检查。如需要检查关联云资源 (需授权服务角色SSL_QCSLinkedRoleInReplaceLoadCertificate)，完成授权后，删除将变成异步任务，接口会返回异步任务ID。需搭配 DescribeDeleteCertificatesTaskResult接口使用，查询删除任务是否成功。 
+     * @return IsSync 删除时是否检查证书关联了云资源。默认不检查。如需要检查关联云资源 (需授权服务角色SSL_QCSLinkedRoleInReplaceLoadCertificate)，完成授权后，删除将变成异步任务，接口会返回异步任务ID。需搭配 DescribeDeleteCertificatesTaskResult接口使用，查询删除任务是否成功。
+     */
+    public Boolean getIsSync() {
+        return this.IsSync;
+    }
+
+    /**
+     * Set 删除时是否检查证书关联了云资源。默认不检查。如需要检查关联云资源 (需授权服务角色SSL_QCSLinkedRoleInReplaceLoadCertificate)，完成授权后，删除将变成异步任务，接口会返回异步任务ID。需搭配 DescribeDeleteCertificatesTaskResult接口使用，查询删除任务是否成功。
+     * @param IsSync 删除时是否检查证书关联了云资源。默认不检查。如需要检查关联云资源 (需授权服务角色SSL_QCSLinkedRoleInReplaceLoadCertificate)，完成授权后，删除将变成异步任务，接口会返回异步任务ID。需搭配 DescribeDeleteCertificatesTaskResult接口使用，查询删除任务是否成功。
+     */
+    public void setIsSync(Boolean IsSync) {
+        this.IsSync = IsSync;
+    }
+
     public DeleteCertificatesRequest() {
     }
 
@@ -31,6 +77,15 @@ public class DeleteCertificatesRequest extends AbstractModel {
      *       and any explicit key, i.e Foo, set via .setFoo("value") will be a deep copy.
      */
     public DeleteCertificatesRequest(DeleteCertificatesRequest source) {
+        if (source.CertificateIds != null) {
+            this.CertificateIds = new String[source.CertificateIds.length];
+            for (int i = 0; i < source.CertificateIds.length; i++) {
+                this.CertificateIds[i] = new String(source.CertificateIds[i]);
+            }
+        }
+        if (source.IsSync != null) {
+            this.IsSync = new Boolean(source.IsSync);
+        }
     }
 
 
@@ -38,6 +93,8 @@ public class DeleteCertificatesRequest extends AbstractModel {
      * Internal implementation, normal users should not use it.
      */
     public void toMap(HashMap<String, String> map, String prefix) {
+        this.setParamArraySimple(map, prefix + "CertificateIds.", this.CertificateIds);
+        this.setParamSimple(map, prefix + "IsSync", this.IsSync);
 
     }
 }
